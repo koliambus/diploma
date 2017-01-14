8e1234a9140c47ddb2ad6831e73e8daf52a4adf2
koliambus <koliambus@ukr.net>
2017-01-14 14:12:45 +0200

    NOT STABLE; add containernumber parameter

--- hello.py
+++ hello.py
@@ -1,10 +1,33 @@
 # -*- coding: utf-8 -*-
+import optparse
+import requests
 from flask import Flask
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
-    return "Flask Dockerized"
+    return "Flask Dockerized, my container number = " + this_container
+
+
+def parse_container():
+    parser = optparse.OptionParser()
+    parser.add_option("-C", "--container", dest="container", default=0)
+    options, _ = parser.parse_args()
+    return options.container
+
 
 if __name__ == "__main__":
+    this_container = parse_container()
     app.run(host='0.0.0.0')
+
+
+@app.route("/hello/<int:container>")
+def hello(container):
+    return "Hello from container #" + container + " through container #" + this_container
+
+
+@app.route("/connect/<int:port>")
+def connect(port):
+    return requests.get("192.168.0.104:" + port + "/hello/" + this_container)
